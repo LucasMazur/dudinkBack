f22ab3ee4be557479b52a018a7247f4bf054d56e
Lucas Mazur <luhcsfezur@yahoo.com>
2021-01-14 15:41:28 -0300

    adicionado rota update

--- API/User.js
+++ API/User.js
@@ -15,7 +15,7 @@ route.post('/save', async(req, res) => {
     await userModel.save((err) => {
         if (!err) console.log("Save Succeful!!") 
         else {
-            console.log("Data Base err")
+            res.send("Data Base err")
         }
     })
     res.json(userModel)
@@ -26,7 +26,7 @@ route.get('/get', async (req, res) => {
         if (!err) {
             res.json(doc)
         } else {
-            console.log("Data Base Err")
+            res.send("Data Base err")
         }
         
     })
@@ -40,8 +40,40 @@ route.post('/remove', async (req, res) => {
             await doc.remove()
             res.json(doc)
         } else {
-            console.log("Data Base Err")
+            res.send("Data Base Err")
         }
+    })
+})
+
+route.post('/update', async (req, res) => {
+    const { name, date, body, size, id } = req.body
+    let user = {}
+    user.id = id
+    user.name = name
+    user.date = date
+    user.body = body
+    user.size = size
+    await User.findById(user.id, async (err, doc) => {
+        if (name === "0") {
+            user.name = doc.name
+        }
+        if (date === "0") {
+            user.date = doc.date
+        }
+        if (body === "0") {
+            user.body = doc.body
+        }
+        if (size === "0") {
+            user.size = doc.size
+        }
+        await User.findByIdAndUpdate(user.id, {"name":user.name, "date":user.date, "body":user.body, "size":user.size}, async (err, doc) => {
+            if (!err) {
+                console.log("Update Succeful!!")
+                res.json(doc)
+            } else {
+                res.send("Data Base Err")
+            }
+        })
     })
 })
 
